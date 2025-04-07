5a54a7dd6db307a3a8fd62dcbdd5ddfe4b9bcb3c
Smanivannan <sachinmanivannan@icloud.com>
2025-04-07 13:53:10 -0400

    Update API URL in environment.prod.ts to new IP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiGatewayUrl: 'http://34.48.110.246:8080/api'  // Use your GCP IP here
+  apiGatewayUrl: 'http://34.45.217.220:8080/api'  // Use your GCP IP here
 };
